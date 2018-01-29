f871876e8b3183c76566dfc9829aa4ae18f9bf2f
lager6 <lager6@gmail.de>
2018-01-29 12:18:29 +0100

    form handling

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from . import app
+from .forms import EmailPasswordForm
 
 
-@app.route('/')
-def index():
-    return "Hallo Welt"
+@app.route('/', methods=["GET", "POST"])
+def login():
+    form = EmailPasswordForm()
+    if form.validate_on_submit():
+
+        print("validated")
+
+        return redirect(url_for('dashboard'))
+    return render_template('index.html', form=form)
+
+
+@app.route('/dashboard')
+
+def dashboard():
+    return render_template('dashboard.html')
